380110d79dcfa4a6a63f8130cc4ec401998dd385
Gorobzeev <staszp@gmail.com>
2015-05-05 23:26:53 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.io.BufferedReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (args[0].equals("create")){
+
+        }
+
+
+    }
+}
