c0f34b29825cd4b67b2c80cbe1fcfe8321093910
Gorobzeev <staszp@gmail.com>
2015-05-07 00:38:07 +0300

    create, delete done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,46 @@
 package com.company;
 
-import java.io.BufferedReader;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args[0].equals("create")){
+            String fileName = args[1];
+            File file = new File("D:/MyProjects/FileManager/"+fileName);
+            file.createNewFile();
+            System.out.println("Файл "+args[1]+"создан.");
+        }
+        else if (args[0].equals("delete")){
+            File dir = new File(args[1]);
+            if (dir.isDirectory()){
+                System.out.println(args[1]+" является каталогом.");
+                File[] flist = dir.listFiles();
+                System.out.println("В каталоге " + args[1]+" - "+flist.length+ " файл(ов).");
+                if (flist.length == 0){
+                    System.out.println("Каталог "+args[1]+" пуст.");
+                    dir.delete();
+                    System.out.println("Каталог "+args[1]+" удален.");
+                    return;
+                }
+                else{
+                    for (int i=0; i<flist.length; i++){
+                        flist[i].delete();
+                    }
+                    System.out.println("Удалено "+flist.length+" файл(ов) из каталога "+args[1]+".");
+                }
+                dir.delete();
+                System.out.println("каталог "+args[1]+" удалена.");
+                return;
+            }
+            dir.delete();
+            System.out.println("Файл "+args[1]+" удален.");
+            return;
+        }
+        else if (args[0].equals("renamme")){
 
         }
-
-
     }
 }
